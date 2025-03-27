7b50586f63abad228e2292b6b05651e4e6a44048
sidra siddique <sidrasiddique494@gmail.com>
2025-03-27 22:41:44 +0500

    changes added

--- basic.java
+++ basic.java
@@ -66,3 +66,6 @@ public class Main {
     public static void main(String[] args) {
        int[] num = {1, 2, 3, 4, 5};
        int max = num[0];
+       for (int i = 1; i < num.length; i++) {
+            max = Math.max(max, num[i]);
+        }
